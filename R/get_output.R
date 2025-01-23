5a82ff24496febf473f5b7f63540642e7f538bee
pete-hulson <pete.hulson@noaa.gov>
2025-01-22 19:52:25 -0900

    add no ageing error output

--- R/get_output.R
+++ R/get_output.R
@@ -19,3 +19,26 @@ iss <- vroom::vroom(here::here('output', 'nebs', 'ftnirs_iss_ag.csv'), delim = '
   tidytable::left_join(vroom::vroom(here::here('output', 'nebs', 'conv_iss_ag.csv'), delim = ',') %>% 
                          tidytable::filter(sex == 4) %>% # filtering to combined sexes post-expansion
                          tidytable::select(year, species_code, iss_conv = iss))
+
+# age comp iss output (no ageing error) ----
+iss_noae <- vroom::vroom(here::here('output', 'nebs', 'ftnirs_noae_iss_ag.csv'), delim = ',') %>% 
+  tidytable::filter(sex == 4) %>% # filtering to combined sexes post-expansion
+  tidytable::select(year, species_code, iss_ftnirs = iss) %>% 
+  tidytable::left_join(vroom::vroom(here::here('output', 'nebs', 'conv_noae_iss_ag.csv'), delim = ',') %>% 
+                         tidytable::filter(sex == 4) %>% # filtering to combined sexes post-expansion
+                         tidytable::select(year, species_code, iss_conv = iss))
+
+iss %>% 
+  tidytable::pivot_longer(cols = c(iss_ftnirs, iss_conv)) %>% 
+ggplot(aes(x = year, y = value, color = name)) +
+  geom_line() +
+  geom_point() +
+  facet_wrap(~species_code, ncol = 1)
+
+iss_noae %>% 
+  tidytable::pivot_longer(cols = c(iss_ftnirs, iss_conv)) %>% 
+  ggplot(aes(x = year, y = value, color = name)) +
+  geom_line() +
+  geom_point() +
+  facet_wrap(~species_code, ncol = 1)
+
